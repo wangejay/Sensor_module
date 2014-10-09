342429de1f82576c0ea932ae2000e1290ad4895f
wangejay <wangejay@gmail.com>
2014-10-09 16:04:32 +0800

    test

--- Light_trigger_capture/Light_trigger_capture.ino
+++ Light_trigger_capture/Light_trigger_capture.ino
@@ -1,13 +1,13 @@
 
 // HCSR04Ultrasonic/examples/UltrasonicDemo/UltrasonicDemo.pde
-#include <Ultrasonic.h>
+//#include <Ultrasonic.h>
 
 #define US_TRIGGER_PIN  4
 #define US_ECHO_PIN     3
 #define PHOTOCELL_PIN 2; // 光敏電阻 (photocell) 接在 anallog pin 2
 int photocellVal = 0; // photocell variable
 
-Ultrasonic ultrasonic(US_TRIGGER_PIN, US_ECHO_PIN);
+//Ultrasonic ultrasonic(US_TRIGGER_PIN, US_ECHO_PIN);
 
 void setup()
 {
@@ -24,11 +24,11 @@ void Capture()
 
 void loop()
 {
-  float cmMsec, inMsec;
-  long microsec = ultrasonic.timing();
+  //float cmMsec, inMsec;
+  //long microsec = ultrasonic.timing();
 
-  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM); // 計算距離&#65292;單位: 公分
-  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN); // 計算距離&#65292;單位: 英吋
+  //cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM); // 計算距離&#65292;單位: 公分
+  //inMsec = ultrasonic.convert(microsec, Ultrasonic::IN); // 計算距離&#65292;單位: 英吋
   
 
   // 讀取光敏電阻並輸出到 Serial Port 
@@ -37,7 +37,7 @@ void loop()
   Serial.println(photocellVal);  
 
   delay(10);
-  if ( photocellVal> 80)
+  if ( photocellVal> 50)
   {
     Capture(); 
     Serial.print("HUGH?");
@@ -48,11 +48,4 @@ void loop()
      Serial.print("LOW?");
   digitalWrite(13, LOW); 
   }
-  if (cmMsec < 1)
-  {
-   digitalWrite(13, HIGH); 
-   delay(10);
-   digitalWrite(13, LOW); 
-   delay(10);
-   }
 }
